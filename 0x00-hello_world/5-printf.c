3bef39295c557a30a0a963cfec2c12d28e3002a2
Ononiwu Benjamin <ononiwubenjamin@yahoo.com>
2023-02-16 14:54:11 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print the c program using printf
+ * main - print a c program using printf
  *
  * Return: 0
 */
@@ -8,3 +8,4 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}
