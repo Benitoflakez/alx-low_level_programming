d5e2ea33d063d45b9faa81f7cfcea9bd6fd8a9c1
Ononiwu Benjamin <ononiwubenjamin@yahoo.com>
2023-02-16 16:02:34 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,5 +17,6 @@ int main(void)
 	printf("size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
 	printf("size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
 	printf("size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
+	fprintf(stderr, "Anything");
 	return (0);
 }
